5e2044dee6b378fec9c19c695ce67d139ff67f52
Simeon Wuthier <simeonwuthier@gmail.com>
2024-02-25 12:50:33 -0700

    Add support for selection of ranges, e.g., directories 3-8.

--- Directory_Selection.py
+++ Directory_Selection.py
@@ -2,8 +2,8 @@ import os
 import re
 import sys
 
-# Given a regular expression, list the directories that match it, and ask for user input
-def selectDir(regex, subdirs = False, multiSelect = False):
+# Given a regular expression, list the directories that match it, and ask for user input to select one or more of them
+def selectDir(regex, subdirs=False, multiSelect=False):
 	try:
 		dirs = []
 		compiledRegex = re.compile(regex)
@@ -26,20 +26,35 @@ def selectDir(regex, subdirs = False, multiSelect = False):
 			print(f'  Directory {i + 1}  -  {directory}')
 		print()
 
-		selectionPrompt = 'Please select directories (e.g., 1,3,5): ' if multiSelect else 'Please select a directory: '
+		selectionPrompt = 'Please select directories (e.g., "1,3-5,9-7,10" for "1,3,4,5,9,8,7,10"): ' if multiSelect else 'Please select a directory: '
 		if multiSelect:
 			selectedDirs = []
 			while not selectedDirs:
 				inputStr = input(selectionPrompt)
 				selections = re.split(r',\s*|\s+', inputStr)
+				addedIndexes = set()
 
 				for selection in selections:
-					try:
-						index = int(selection) - 1
-						if 0 <= index < len(dirs):
-							selectedDirs.append(dirs[index])
-					except ValueError:
-						pass
+					if '-' in selection:
+						parts = selection.split('-')
+						try:
+							start = int(parts[0]) - 1
+							end = int(parts[1]) - 1
+							step = 1 if start <= end else -1
+							for index in range(start, end + step, step):
+								if 0 <= index < len(dirs) and index not in addedIndexes:
+									selectedDirs.append(dirs[index])
+									addedIndexes.add(index)
+						except ValueError:
+							pass
+					else:
+						try:
+							index = int(selection) - 1
+							if 0 <= index < len(dirs) and index not in addedIndexes:
+								selectedDirs.append(dirs[index])
+								addedIndexes.add(index)
+						except ValueError:
+							pass
 
 				if not selectedDirs:
 					print('Invalid selection, please try again.')
@@ -47,9 +62,16 @@ def selectDir(regex, subdirs = False, multiSelect = False):
 			return selectedDirs
 		else:
 			while True:
-				selection = int(input(selectionPrompt))
-				if 1 <= selection <= len(dirs):
-					return dirs[selection - 1]
+				selection = input(selectionPrompt)
+				if '-' in selection:
+					print('Range selection is not supported in single select mode.')
+					continue
+				try:
+					selection = int(selection)
+					if 1 <= selection <= len(dirs):
+						return dirs[selection - 1]
+				except ValueError:
+					print('Invalid selection, please try again.')
 	except KeyboardInterrupt:
 		print("\nOperation cancelled by user.")
 		sys.exit()
